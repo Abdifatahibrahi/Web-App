b00787cc727258d8ffc44a667eceb78572b7ee18
Abdifatah <alsidiq099@gmail.com>
2022-02-14 12:58:32 +0300

    Added new files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = None
+
 def home(request):
-    return HttpResponse('')
+    my_context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html")
+
+def about(request):
+    return render(request, "blog/about.html")
